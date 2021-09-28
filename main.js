810590c5937360cb8ea89136254ef9382d057a06
Nacho <ta.descalzo@gmail.com>
2021-09-28 19:31:50 -0300

    Segundo commit, maquetado semifinalizado y funciones basicas de interaccion

---
+++ main.js
@@ -0,0 +1,17 @@
+let aboutModal = document.querySelector(".about");
+let modalClose = document.querySelector(".about--modal__close");
+let aboutLink = document.querySelector("#aboutLink");
+modalClose.addEventListener("click", (e) => {
+  e.preventDefault();
+  aboutModal.style.opacity = 0;
+  setTimeout(function () {
+    aboutModal.style.display = "none";
+  }, 500);
+});
+aboutLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  aboutModal.style.display = "flex";
+  setTimeout(function () {
+    aboutModal.style.opacity = 1;
+  }, 500);
+});
